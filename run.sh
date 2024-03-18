39895364c40c2401cca5e65f85306edb0c153b7f
simon01222 <156026520+simon01222@users.noreply.github.com>
2024-03-18 22:00:20 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,12 @@
 mvn package
 bin/beast target/group1_projectC-1.0-SNAPSHOT.jar task1 wildfiredb_1k.csv.bz2
 bin/beast target/group1_projectC-1.0-SNAPSHOT.jar task4 wildfiredb_1
+rm -rf wildfireIntensityCounty
+# excecute command, the last command, data file, city file, begin time, end time
+spark-submit \
+--jars $(echo ~/jars/*.jar | tr ' ' ',') \
+--class org.example.Task2 \
+--master local \
+--executor-memory 3G \
+WildfireDataAnalysis-1.0-SNAPSHOT.jar \
+wildfiredb_sample.parquet tl_2018_us_county.zip 01/01/2000 01/01/2024
