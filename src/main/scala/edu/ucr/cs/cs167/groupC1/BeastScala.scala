6b4957fc8cebddb336d203c06abca0665f6caa76
simon01222 <156026520+simon01222@users.noreply.github.com>
2024-03-18 21:52:01 +0000

    Update BeastScala.scala

--- src/main/scala/edu/ucr/cs/cs167/groupC1/BeastScala.scala
+++ src/main/scala/edu/ucr/cs/cs167/groupC1/BeastScala.scala
@@ -1,5 +1,4 @@
 package edu.ucr.cs.cs167.groupC1
-
 import edu.ucr.cs.bdlab.beast.geolite.{Feature, IFeature}
 import org.apache.spark.beast.SparkSQLRegistration
 import org.apache.spark.rdd.RDD
@@ -12,7 +11,11 @@ import org.apache.spark.ml.evaluation.RegressionEvaluator
 import org.apache.spark.ml.Pipeline
 import org.apache.spark.sql.functions.{col, to_date, year, month, avg, sum}
 import org.apache.spark.rdd.RDD
-
+import org.apache.spark.beast.SparkSQLRegistration
+import org.apache.spark.sql.SparkSession
+import java.text.SimpleDateFormat
+import org.apache.commons.lang3.time.DateUtils
+import org.apache.spark.SparkConf
 
 
 /**
@@ -109,6 +112,67 @@ object BeastScala {
         //TASK 2:
         case "task2" =>
           val t3 = System.nanoTime()
+        def init(spark: SparkSession): Unit = {
+    // Start the CRSServer and store the information in SparkConf
+    val sparkContext = spark.sparkContext
+    //    CRSServer.startServer(sparkContext)
+    SparkSQLRegistration.registerUDT
+    SparkSQLRegistration.registerUDF(spark)
+  }
+
+  def doTask(spark: SparkSession, dataPath: String, countryPath: String, startDate: String, endDate: String): Unit = {
+    println(s"document file => ${dataPath}")
+    var dataDF = spark.read.parquet(dataPath)
+    dataDF.show()
+    println(s"struct1: ${dataDF.dtypes.toSeq}")
+    dataDF.createOrReplaceTempView("tmp_data")
+
+    val newColumnStr = dataDF.columns.map(c => if (Seq("acq_date").contains(c)) s"to_date(${c}) $c" else c).mkString(",")
+    dataDF = spark.sql(s"select ${newColumnStr} from tmp_data")
+    println(s"struct2: ${dataDF.dtypes.toSeq}")
+    dataDF.createOrReplaceTempView("tmp_data")
+    spark.sql(s"select count(*) cnt,min(acq_date) min_date,max(acq_date) max_date from tmp_data").show()
+
+    println(s"city file => ${countryPath}")
+    val countiesDF = spark.sparkContext.shapefile(countryPath).toDataFrame(spark)
+    countiesDF.show()
+    countiesDF.createOrReplaceTempView("tmp_country")
+    val sql =
+      s"""
+         |select b.GEOID,b.NAME,b.g,a.fire_intensity from (
+         |  select County,sum(frp) fire_intensity from tmp_data where acq_date BETWEEN '${startDate}' AND '${endDate}' group by County
+         |) a inner join tmp_country b on a.County = b.GEOID
+         |""".stripMargin.trim
+    println(s"excecute SQL => ${sql}")
+    val result = spark.sql(sql)
+    result.show()
+    result.toSpatialRDD.coalesce(1).saveAsShapefile("wildfireIntensityCounty")
+  }
+
+  val simple = new SimpleDateFormat("yyyy-MM-dd")
+
+  def formatDate(str: String): String = {
+    val date = DateUtils.parseDate(str, Array("yyyy-MM-dd", "MM/dd/yyyy"): _*)
+    simple.format(date)
+  }
+
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf().setAppName("Analysis")
+    if (!conf.contains("spark.master"))
+      conf.setMaster("local[*]")
+    val spark = SparkSession.builder().config(conf).getOrCreate()
+    println(s"parameter => ${args.toSeq}")
+    val dataPath = args(0)
+    val countryPath = args(1)
+    val startDate = formatDate(args(2))
+    val endDate = formatDate(args(3))
+    init(spark)
+    doTask(spark, dataPath, countryPath, startDate, endDate)
+    spark.stop()
+  }
+
+}
+
 
 
 
