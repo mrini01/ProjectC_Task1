edcc966dcf7a9db75fb84d3db0a42c50e9412730
osugi001 <102548267+osugi001@users.noreply.github.com>
2024-03-18 18:21:46 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 mvn package
 bin/beast target/group1_projectC-1.0-SNAPSHOT.jar task1 wildfiredb_1k.csv.bz2
-bin/beast target/group1_projectC-1.0-SNAPSHOT.jar task4 wildfiredb_1
+bin/beast target/group1_projectC-1.0-SNAPSHOT.jar task4 wildfiredb_ZIP
 rm -rf wildfireIntensityCounty
 # excecute command, the last command, data file, city file, begin time, end time
 spark-submit \
