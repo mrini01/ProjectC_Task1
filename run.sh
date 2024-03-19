3461e7478ec1734ad20f4f37d8b56ea2211c7fc2
simon01222 <156026520+simon01222@users.noreply.github.com>
2024-03-19 10:01:45 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,5 +3,4 @@ bin/beast target/group1_projectC-1.0-SNAPSHOT.jar task1 wildfiredb_1k.csv.bz2
 bin/beast target/group1_projectC-1.0-SNAPSHOT.jar task4 wildfiredb_ZIP
 rm -rf wildfireIntensityCounty
 # excecute command, the last command, data file, city file, begin time, end time
-rm -rf wildfireIntensityCounty
 bin/beast target/group1_projectC-1.0-SNAPSHOT.jar task2 wildfiredb_sample.parquet tl_2018_us_county.zip 01/01/2000 01/01/2024
